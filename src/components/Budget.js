637939ecabe72e035ad1b0c9d8b2500c304501ed
Karina Lubinets <lubineckarina@gmail.com>
2023-05-06 07:10:40 -0400

    Budget allocation validation & Editable budget value added
    Signed-off-by: Karina Lubinets <lubineckarina@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,38 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch, expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
+    const updateBudget = (event) => {
+        const budgetValue = event.target.value === "" ? 0 : parseInt(event.target.value);
+        if (budgetValue > 20000) {
+          alert("The value cannot exceed £20000");
+          dispatch({ type: 'SET_BUDGET', payload: "20000" });
+        } else if (budgetValue < totalExpenses) {
+          alert(`You cannot reduce the budget value lower than the current spending (${totalExpenses})`);
+          dispatch({ type: 'SET_BUDGET', payload: totalExpenses });
+        } else {
+          dispatch({ type: 'SET_BUDGET', payload: budgetValue });
+        }
+      }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input
+                            type="number"
+                            defaultValue={budget}
+                            step="10"
+                            min={totalExpenses}
+                            max="20000"
+                            id="number" 
+                            onInput = {(event) => updateBudget(event)}>
+                            </input>
+            </span>
         </div>
     );
 };
